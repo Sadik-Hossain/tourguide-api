6e26f9569a96429ed72f74da5f1ee2bf1f79e3ad
sadik <po480373@gmai.com>
2022-09-23 12:44:37 +0600

    connection cofirmation

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connect = async () => {
   mongoose.connect(process.env.DB);
   mongoose.connection
-    .once("open", () => console.log(mongoose.connection.client))
+    .once("open", () => console.log(mongoose.connection.name))
     .on("error", (error) => {
       console.log(`your error,${error}`);
     });
